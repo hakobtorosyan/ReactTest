e971d53e55b4608ef691f439d1a2e499f3ff2675
hakobtorosyan <torosyanhak@gmail.com>
2017-05-10 09:22:47 +0400

    tenanq

---
+++ test-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InputList from './InputList';
+
+
+ReactDOM.render(
+    <InputList />,
+    document.getElementById('root')
+);
