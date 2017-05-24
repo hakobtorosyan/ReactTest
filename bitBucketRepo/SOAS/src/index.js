1e41dcf0fbf50341b233cb6548aaf218b2a62dbd
hakobtorosyan <torosyanhak@gmail.com>
2017-05-24 17:54:19 +0400

    deleted trash 2

--- bitBucketRepo/SOAS/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import WrapperDiv from './components/WrapperDiv';
-
-ReactDOM.render(
-  <WrapperDiv />,
-  document.getElementById('myApp')
-);
--- test-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import InputList from './InputList';
-
-
-ReactDOM.render(
-    <InputList />,
-    document.getElementById('root')
-);
